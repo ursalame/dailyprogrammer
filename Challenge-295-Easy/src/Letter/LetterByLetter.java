1f683d2458db784eab67b85c45697bdeddbac9a2
desktop <salame.roy@courrier.uqam.ca>
2016-12-28 01:09:56 -0500

    [2016-12-12] Challenge #295 [Easy] Letter by letter, updated letter by letter to allow more strategies

--- Challenge-295-Easy/src/Letter/LetterByLetter.java
+++ Challenge-295-Easy/src/Letter/LetterByLetter.java
@@ -13,20 +13,33 @@ public class LetterByLetter {
         this.letterComparator = new LetterComparator();
     }
 
-    public boolean hasNext(){
+    public boolean hasNext() {
         return current.hasNext();
     }
 
-    public LetterSequence transformNext(){
-        if(this.hasNext() && letterComparator.compare(current.next(),end.next()) != 0){
+    public LetterSequence transformNext() {
+        if (this.hasNext() && letterComparator.compare(current.next(), end.next()) != 0) {
             current.setCurrentLetter(end.getCurrentLetter());
             return current;
         }
         return null;
     }
 
-    public void reset(){
+    public void reset() {
         current = start;
         end.resetIndex();
+    }
+
+    public void setIndex(int i) {
+        current.setIndex(i);
+        end.setIndex(i);
+    }
+
+    public LetterSequence changeCurrentLetter() {
+        if (letterComparator.compare(current.getCurrentLetter(), end.getCurrentLetter()) != 0
+                && current.setCurrentLetter(end.getCurrentLetter())) {
+            return current;
+        }
+        return null;
     }
 }
